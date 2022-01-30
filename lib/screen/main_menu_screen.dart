0ec35f88ae9bc70b1521cce8c22af98498cb1137
jlprince21 <luke@theprinces.org>
2022-01-30 07:15:51 -0500

    Add database export functionality

--- lib/screen/main_menu_screen.dart
+++ lib/screen/main_menu_screen.dart
@@ -1,6 +1,9 @@
+
 import 'package:flutter/material.dart';
-import 'package:thing_finder/screen/items_screen.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:thing_finder/screen/containers_screen.dart';
+import 'package:thing_finder/screen/items_screen.dart';
 
 class MainMenuScreen extends StatefulWidget {
   const MainMenuScreen({Key? key}) : super(key: key);
@@ -41,6 +44,18 @@ class _MainMenuScreenState extends State<MainMenuScreen> {
                   ),
                 ),
               ),
+            Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    child: Text('Export DB'),
+                    onPressed: () {
+                      exportDatabase();
+                    },
+                  ),
+                ),
+              ),
+
             ],
           ),
         ),
@@ -58,5 +73,13 @@ class _MainMenuScreenState extends State<MainMenuScreen> {
         style: Theme.of(context).textTheme.headline5,
       ),
     );
+  }
+
+  exportDatabase() async {
+    // TODO this method may be more useful in database area
+    var appDocumentsDirectory = await getApplicationDocumentsDirectory();
+    var appDocumentsPath = appDocumentsDirectory.path;
+    var filePath = '$appDocumentsPath/db.sqlite'; // TODO this path probably needs to be a constant somewhere
+    Share.shareFiles([filePath]);
   }
 }
